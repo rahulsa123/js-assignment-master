400e2837c4999656f1d3baea961928b5e4a915ea
Rahul Sahu <rahul.sahu3@kotak.com>
2024-05-26 16:16:46 +0530

    Added support for matchId,matchFormat,matchStartTime in /sport/tour/match

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,14 +4,19 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchName, matchId, matchFormat, matchStartTime } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({
+            matchId,
+            matchName,
+            matchFormat,
+            matchStartTime
+        });
     });
     return res;
 }
--- src/models/sport.js
+++ src/models/sport.js
@@ -1,7 +1,8 @@
 const mysql = require('../lib/mysql');
 
 const getAllSportsToursAndMatches = async () => {
-    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName ' +
+    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName, m.id as matchId,' +
+        'm.format as matchFormat, m.startTime as matchStartTime ' +
         'from matches m inner join tours t on m.tourId = t.id ' +
         'inner join sports s on t.sportId = s.id';
     const parameters = [];
