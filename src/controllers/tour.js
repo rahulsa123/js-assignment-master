159d2ca3eb7de6a8438d0a16387a4d060f86ca99
Rahul Sahu <rahul.sahu3@kotak.com>
2024-05-26 15:53:36 +0530

    Changed SQL join and added support for pagination in /tour/matches endpoint

--- src/controllers/tour.js
+++ src/controllers/tour.js
@@ -5,12 +5,22 @@ const getAllTours = async () => {
 }
 
 const getMatchesByTourName = async params => {
-    const { name } = params;
+    const MAX_PAGE_SIZE = 5;
+    const { name, page_size, page_no } = params;
 
     if (!name) {
         throw new Error('Missing required parameter: name');
     }
-
+    if(page_size && !isNaN(page_size) && page_size>0 && page_size<MAX_PAGE_SIZE){
+        params.page_size = parseInt(page_size)
+    }else{
+        params.page_size = MAX_PAGE_SIZE
+    }
+    if(page_no && !isNaN(page_no) ){
+        params.page_no = parseInt(page_no);
+    }else{
+        params.page_no = 1
+    }
     return await Tour.getMatchesByTourName(params);
 }
 
--- src/models/tour.js
+++ src/models/tour.js
@@ -7,8 +7,22 @@ const getAllTours = async () => {
 }
 
 const getMatchesByTourName = async params => {
-    const statement = 'select * from matches left join tours on matches.tourId = tours.id where tours.name = ?';
-    const parameters = [ params.name ];
+    // Instead of using left join using inner join.
+    // Instead of using * giving alias name to all required column.
+    const statement = 'select tours.id as tourId, ' +
+        'tours.name as tourName, ' +
+        'tours.sportId as sportId, ' +
+        'tours.startTime as tourStartTime, ' +
+        'tours.endTime as tourEndTime, ' +
+        'matches.name as matchName, ' +
+        'matches.format as matchFormat, ' +
+        'matches.startTime as matchStartTime, ' +
+        'matches.endTime as matchEndTime ' +
+        ' from matches join tours on matches.tourId = tours.id where tours.name = ? ' +
+        'LIMIT ? OFFSET ?';
+    const parameters = [params.name, parseInt(params.page_size), parseInt(params.page_no - 1)
+    * parseInt(params.page_size)];
+
     return await mysql.query(statement, parameters);
 }
 
